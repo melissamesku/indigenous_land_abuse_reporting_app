9f90b1d18be85e9cbf57a6710260c5b5f2b2bc81
Melissa Mesku <mmesku@gmail.com>
2015-11-05 12:09:59 -0500

    added devise gem, signup/login functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Melissa is adding this line as per the rails guide for using the devise gem
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
