0d062f72f996e3244b9515b80456bb338807a774
Melissa Mesku <mmesku@gmail.com>
2015-11-07 16:46:56 -0500

    added update and destroy routes' pages

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -25,13 +25,14 @@ class ReportsController < ApplicationController
   def create
   	@report = Report.new(report_params)
   	if @report.save
-  		redirect_to reports_path
+  		redirect_to reports_path, :notice => "Your report has been saved."
   	else 
   		render :new
   	end
   end
 
   def edit
+  	@report = Report.find(params[:id])
   	# @report = Report.new
   	# if @report[:user_id] != current_user[:id]
   	# 	redirect_to root_path
@@ -41,13 +42,16 @@ class ReportsController < ApplicationController
   def update
   	@report = Report.find(params[:id])
   	if @report.update(report_params)
-  		redirect_to report_path
+  		redirect_to reports_path, :notice => "Your report has been updated." 
   	else
-  		render :new
+  		render :edit
   	end
   end
 
   def destroy
+  	@report = Report.find(params[:id])
+  	@report.destroy
+  	redirect_to reports_path, :notice => "Your report has been deleted."
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ Report.create({
   user_id: 2
 })
 Report.create({
-  headline: "Seed test", 
-  details: "Seed test.",
-  location: "Seed test, Brazil",
-  image: "http://dingo.care2.com/pictures/greenliving/uploads/2010/12/deforestation-tractor.jpg",
-  user_id: 2
+  headline: "Aribola, Brazil intentionally set aflame by illegal loggers", 
+  details: "Illegal loggers in the Aribola region of the Brazilian Amazon have set areas of this protected forest on fire in retaliation for the Guajajara's attempts to protect their land. Thirty indigenous firefighters have been combatting the flames while the government has failed to provide assistance. \n UPDATE: Local government has sent firefighters to help put out the blaze. The firefighters have been attacked by illegal loggers and the situation has escalated and now has limited international news coverage.",
+  location: "Aribola, Brazilian Amazon",
+  image: "http://questgarden.com/96/70/9/100224151836/images/rainforest-destruction.jpg",
+  user_id: 3
 })
