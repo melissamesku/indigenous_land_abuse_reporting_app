e9873204ddec650eec2ff77e6a19ac118f7dfcaa
Melissa Mesku <mmesku@gmail.com>
2015-11-08 16:15:23 -0500

    added validation

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,8 @@
 class Report < ActiveRecord::Base
 	belongs_to :users
+
+	validates :headline, :details, :presence => true
+	validates :headline, :length => {:minimum => 2}
+	validates :headline, :uniqueness => true
+
 end
