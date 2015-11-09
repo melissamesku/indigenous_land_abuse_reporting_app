644d6f77a18766a07a1111e0ae77400411aa9589
Melissa Mesku <mmesku@gmail.com>
2015-11-08 19:38:47 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,56 @@ Report.create({
   headline: "New wave of illegal gold miners dumping mercury in Yanomami rivers", 
   details: "Illegal gold mining has been happening in the Yanomami's homeland, the Alto Orinoco – Casiquiare Biosphere Reserve, Venezuela, since the 1970s. But a new wave of illegal gold miners is swarming the area bringing with them malaria and other disease, and dumping mercury in the rivers. Additionally there are uncontacted tribes in the forest who are at particular risk as they live close to where the miners are intruding.",
   location: "Casiquiare Biosphere Reserve, Venezuela 10.5000° N, 66.9667° W",
-  image: "http://dingo.care2.com/pictures/greenliving/uploads/2010/12/deforestation-tractor.jpg",
+  image: "http://blogs.nelson.wisc.edu/es112-309-3/wp-content/uploads/sites/68/2014/02/mine.jpg",
   user_id: 2
 })
 Report.create({
-  headline: "Aribola, Brazil intentionally set aflame by illegal loggers", 
-  details: "Illegal loggers in the Aribola region of the Brazilian Amazon have set areas of this protected forest on fire in retaliation for the Guajajara's attempts to protect their land. Thirty indigenous firefighters have been combatting the flames while the government has failed to provide assistance. In under two months, 45% of Guajajara-held lands have burned. \n UPDATE: Local government has sent firefighters to help put out the blaze. The firefighters have been attacked by illegal loggers and the situation has escalated and now has limited international news coverage.",
-  location: "Aribola, Brazilian Amazon",
+  headline: "Arariboia , Brazil intentionally set aflame by illegal loggers", 
+  details: "Illegal loggers in the Arariboia region of the Brazilian Amazon have set areas of this protected forest on fire in retaliation for the Guajajara's attempts to protect their land. Thirty indigenous firefighters have been combatting the flames while the government has failed to provide assistance. In under two months, 45% of Guajajara-held lands have burned. \n UPDATE: Local government has sent firefighters to help put out the blaze. The firefighters have been attacked by illegal loggers and the situation has escalated and now has limited international news coverage.",
+  location: "Jucaral village, Araribóia Indigenous Reserve, Maranhão, Brazil",
   image: "http://www.greenpeace.org.uk/files/images/forests/amazon/amazon-fires-aug08.jpg",
   user_id: 3
 })
 
-Davi Kopenawa
+
+http://dingo.care2.com/pictures/greenliving/uploads/2010/12/deforestation-tractor.jpg
+
+http://blogs.nelson.wisc.edu/es112-309-3/wp-content/uploads/sites/68/2014/02/mine.jpg
 
 http://questgarden.com/96/70/9/100224151836/images/rainforest-destruction.jpg
+
+Report.create({
+  headline: "5000 Hectares stolen to grow palm oil illegally", 
+  details: "Activist leader Washington Bolivar said, 'Our lands have been devastated, all the forest is gone, and the streams are completely churned up and blocked, there is now only one stream we can still use for clean drinking water.' In just three years, over 5200 hectares of ancestral forest has been destroyed. The Peruvian government has failed to fully recognize the community's rightful claim to their land. The government sold the parcel of land to Plantaciones de Pucallpa in 2012, though it wass technically illegal to do so, given that all conversion of primary forest, which is about 80% of that area, is expressly forbidden by law.",
+  location: "Shipibo community of Santa Clara de Uchunya, Peru",
+  image: "http://mms.businesswire.com/media/20150407006063/en/461400/4/Tamshiyacu.jpg",
+  user_id: 3
+})
+
+Report.create({
+  headline: "Isolated Awa in Brazil Forced Out of Their Lands Again by Illegal Loggers", 
+  details: "Illegal logging is forcing isolated indigenous people out of their territory again in Maranhao province in northern Brazil, renewing concerns of further deaths from assaults and exposure to western diseases among other things according to Survival International (SI) and other advocates.",
+  location: "Awa territory, Maranhao, Brazil",
+  image: "http://i.huffpost.com/gen/1249169/original.jpg",
+  user_id: 3
+})
+
+Report.create({
+  headline: "Illegal logging continues even after land rights have been granted", 
+  details: "It has been one year since the murder of Edwin Chota and three other Asháninka natives by a group of illegal loggers. Since then, some Asháninka communities were presented with land titles, but illegal loggers are still present.",
+  location: "Saweto Ucayali, Peru",
+  image: "http://mms.businesswire.com/media/20150407006063/en/461400/4/Tamshiyacu.jpg",
+  user_id: 3
+})
+
+Report.create({
+  headline: "Illegal logging continues even after land rights have been granted", 
+  details: "It has been one year since the murder of Edwin Chota and three other Asháninka natives by a group of illegal loggers. Since then, some Asháninka communities were presented with land titles, but illegal loggers are still present.",
+  location: "peatlands in Palangka Raya, Central Kalimantan, Indonesia",
+  image: "http://en.citizendaily.net/wp-content/uploads/2015/11/greenpeace-oil-palms-burnt-land-1.jpeg",
+  user_id: 3
+})
+
+
+
+
